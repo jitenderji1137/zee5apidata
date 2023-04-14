f3f9f3bdb447e50830bf3cb6e8f89d96a20c9b00
jitender <113350806+jitenderji1137@users.noreply.github.com>
2023-04-14 09:29:50 +0530

    Create WhatsApp.js

---
+++ WhatsApp.js
@@ -0,0 +1,36 @@
+<script>
+    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?47835';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    var options = {
+  "enabled":true,
+  "chatButtonSetting":{
+      "backgroundColor":"#ff0000",
+      "ctaText":"FREE NETFLIX",
+      "borderRadius":"20",
+      "marginLeft":"0",
+      "marginBottom":"50",
+      "marginRight":"50",
+      "position":"right"
+  },
+  "brandSetting":{
+      "brandName":"FREE NETFLIX",
+      "brandSubTitle":"Typically replies within a 12hr",
+      "brandImg":"https://i.postimg.cc/wxNWxtMY/rounded-corners-removebg-preview.png",
+      "welcomeText":"Hi there! Welcome to Free Netflix\nHow can I help you?",
+      "messageText":"Type your Query here . . .",
+      "backgroundColor":"#000000",
+      "ctaText":"Send",
+      "borderRadius":"10",
+      "autoShow":false,
+      "phoneNumber":"918307056141"
+  }
+};
+    s.onload = function() {
+        CreateWhatsappChatWidget(options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+</script>
